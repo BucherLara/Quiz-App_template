6bc3adf13b629290f185d6025813fa0418d3c86a
Lara Bucher <bucher.lara@web.de>
2022-10-07 15:47:55 +0200

    add bookmark-toggle andbutton-toggle

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
 const card = document.querySelector('[data-js="card"]');
+const cardAnswer = document.querySelector('[data-js="card__answer"]');
+const answerButton = document.querySelector('[data-js="answer-button"]');
+const bookmarkButton = document.querySelector('[data-js="button__bookmark"]');
 
-const answerButton = docuemnt.querySelector('[data-js="answer-button"]');
+answerButton.addEventListener("click", (event) => {
+  console.log(event.target);
+  cardAnswer.classList.toggle("card__answer--active");
+  if (cardAnswer.classList.contains("card__answer--active")) {
+    event.target.textContent = "Hide answer";
+  } else {
+    event.target.textContent = "Show answer";
+  }
+});
+
+bookmarkButton.addEventListener("click", (event) => {
+  console.log(event.target);
+  event.target.classList.toggle("bookmark--active");
+});
